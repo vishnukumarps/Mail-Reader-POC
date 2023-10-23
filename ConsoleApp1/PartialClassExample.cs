335388c5821f92bb67df7f5bfefc7f7a902cfbee
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2023-10-23 11:18:44 +0530

    tuple

--- ConsoleApp1/PartialClassExample.cs
+++ ConsoleApp1/PartialClassExample.cs
@@ -1,45 +1,74 @@
-﻿using namespace1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using namespace1;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp1
-{
-    internal class PartialClassExample
-    {
-        static void Main(string[] args)
-        {
-           Helper helper = new Helper();
-            helper.AddTwoNumbers();
-        }
-    }
-}
+//namespace ConsoleApp1
+//{
+//    internal class PartialClassExample
+//    {
+//        static void Main(string[] args)
+//        {
+//            Helper helper = new Helper();
+//            helper.Hai();
+//            helper.Hello();
+//        }
+//    }
+//}
 
-namespace namespace1
-{
-    public partial class  Helper
-    {
-        int a = 99;
-        public partial void AddTwoNumbers();
-        public partial void AddTwoNumbers()
-        {
-            a = 100;
-            Console.WriteLine( a);
-        }
-    }
-}
+//namespace namespace1
+//{
+//    public partial class Helper
+//    {
+//        int a = 99;
 
-namespace namespace2
-{
-    public partial class Helper
-    {
-        public partial void AddTwoNumbers();
-        public partial void AddTwoNumbers()
-        {
+//        internal void Hai()
+//        {
+//            throw new NotImplementedException();
+//        }
 
-            Console.WriteLine("Hello");
-        }
-    }
-}
+//        partial void AddTwoNumbers();
+
+//    }
+
+//    public partial class Helper
+//    {
+//        internal void Hello()
+//        {
+//            throw new NotImplementedException();
+//        }
+//        partial void AddTwoNumbers()
+//        {
+//            Console.WriteLine("Add two num");
+//        }
+//    }
+
+//    public partial class Helper
+//    {
+//        internal void Hello2()
+//        {
+
+//            var dict = new Dictionary<string, Object>()
+//            {
+//                {"Id",1 }
+//            };
+
+//            dict.TryGetValue("Id",  out var value1);
+
+
+//            int.TryParse("1", out int con);
+//            throw new NotImplementedException();
+//        }
+//        //partial void AddTwoNumbers() // This is not possible
+//        //{
+//        //    Console.WriteLine("Add two num2");
+//        //}
+//    }
+//}
+
+//namespace namespace2
+//{
+
+//}
---
+++ ConsoleApp1/TupleExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TupleExample
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Debugger.Break();
+
+            (string, double, int) someData = ("Apple", 1.5, 2500);
+
+            Console.WriteLine(someData);
+            Tuple<int, int>? tupleData = Tuple.Create(1, 3);
+            Tuple<int, string, bool>? myTuple = new Tuple<int, string, bool>(1, "Hello", true);
+
+
+
+        }
+
+        void Something(Dictionary<string, Tuple<string, double, int>> t)
+        {
+
+        }
+      
+    }
+}
