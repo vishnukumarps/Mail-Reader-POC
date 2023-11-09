8cd35a1c852d4f7eeb743e3cac37617955cf0665
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2023-11-09 11:56:35 +0530

    dictonary delegate example

---
+++ ConsoleApp1/DictionaryDelegateUse.cs
@@ -0,0 +1,42 @@
+﻿
+
+Test();
+
+void Test()
+{
+    IDictionary<int, Action> _operationHandler = new Dictionary<int, Action>()
+            {
+                {1,Sum },
+                {2, Product },
+                {3, Quotient },
+                {4, Subtract },
+            };
+
+    Console.WriteLine("Enter a number");
+    int n=Convert.ToInt32(Console.ReadLine());
+
+    if (_operationHandler.ContainsKey(n))
+    {
+        _operationHandler[n]();
+    }
+}
+ void Sum()
+{
+    Console.WriteLine("Sum");
+}
+void Product()
+{
+    Console.WriteLine("Product");
+}
+ void Quotient()
+{
+    //quotient
+    Console.WriteLine("Quotient");
+}
+
+ void Subtract()
+{
+    Console.WriteLine("Subtract");
+}
+
+
