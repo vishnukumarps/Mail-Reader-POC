cd2b17a028e1d3adf0efe534af6f352c87058914
Vishnu Kumar PS <vishnu.kumarps@claysys.net>
2023-12-28 14:27:50 +0530

    temp

--- ConsoleApp1/DictionaryDelegateUse.cs
+++ ConsoleApp1/DictionaryDelegateUse.cs
@@ -1,51 +1,51 @@
 ﻿
 
-Test();
+//Test();
 
-void Test()
-{
-    IDictionary<int, Action> _operationHandler = new Dictionary<int, Action>()
-            {
-                {1,Sum },
-                {2, Product },
-                {3, Quotient },
-                {4, Subtract },
-            };
+//void Test()
+//{
+//    IDictionary<int, Action> _operationHandler = new Dictionary<int, Action>()
+//            {
+//                {1,Sum },
+//                {2, Product },
+//                {3, Quotient },
+//                {4, Subtract },
+//            };
 
-    var _operationHandler2 = new Dictionary<int,Func<int,int,Task<int>>>()
-            {
-               {22,async(a,b)=>await HandleCreateOrganizationRequest(1,2)}
-            };
-    Console.WriteLine("Enter a number");
-    int n = Convert.ToInt32(Console.ReadLine());
+//    var _operationHandler2 = new Dictionary<int,Func<int,int,Task<int>>>()
+//            {
+//               {22,async(a,b)=>await HandleCreateOrganizationRequest(1,2)}
+//            };
+//    Console.WriteLine("Enter a number");
+//    int n = Convert.ToInt32(Console.ReadLine());
 
-    if (_operationHandler.ContainsKey(n))
-    {
-        _operationHandler[n].Invoke();
-    }
-}
-void Sum()
-{
-    Console.WriteLine("Sum");
-}
-void Product()
-{
-    Console.WriteLine("Product");
-}
-void Quotient()
-{
-    //quotient
-    Console.WriteLine("Quotient");
-}
+//    if (_operationHandler.ContainsKey(n))
+//    {
+//        _operationHandler[n].Invoke();
+//    }
+//}
+//void Sum()
+//{
+//    Console.WriteLine("Sum");
+//}
+//void Product()
+//{
+//    Console.WriteLine("Product");
+//}
+//void Quotient()
+//{
+//    //quotient
+//    Console.WriteLine("Quotient");
+//}
 
-void Subtract()
-{
-    Console.WriteLine("Subtract");
-}
+//void Subtract()
+//{
+//    Console.WriteLine("Subtract");
+//}
 
 
-async Task<int> HandleCreateOrganizationRequest(int a, int b)
-{
+//async Task<int> HandleCreateOrganizationRequest(int a, int b)
+//{
 
-    return await Task.FromResult(1);
-}
+//    return await Task.FromResult(1);
+//}
---
+++ ConsoleApp1/DictonaryTrailAndError.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class DictonaryTrailAndError
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> claims = new Dictionary<string, object>()
+            {
+                {"role_id", 1 },
+                {"root_org_node_ids",new List<int>{ 322,300 } },
+            };
+
+
+            object? rootOrgIdObj = claims.FirstOrDefault(x => x.Key == "root_org_node_ids").Value;
+
+            if (rootOrgIdObj != null)
+            {
+                var convertedIds = (List<int>)rootOrgIdObj;
+
+                foreach (var convertedId in convertedIds)
+                {
+                    Console.WriteLine(convertedId);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
